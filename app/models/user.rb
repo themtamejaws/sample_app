d9c3e622cfd364688669dd75379b509137c0f98e
James Morby <jamesmathewmorby@gmail.com>
2014-04-16 14:41:24 +0100

    make user feed on homepage 1

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,11 @@ class User < ActiveRecord::Base
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence:   true,
-                    format:     { with: VALID_EMAIL_REGEX },
-                    uniqueness: { case_sensitive: false }
+    format:     { with: VALID_EMAIL_REGEX },
+    uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, length: { minimum: 6 }
-  
+
   def User.new_remember_token
     SecureRandom.urlsafe_base64
   end
@@ -18,9 +18,14 @@ class User < ActiveRecord::Base
     Digest::SHA1.hexdigest(token.to_s)
   end
 
+  def feed
+    # This is preliminary. See "Following users" for the full implementation.
+    Micropost.where("user_id = ?", id)
+  end
+
   private
 
-    def create_remember_token
-      self.remember_token = User.hash(User.new_remember_token)
-    end
+  def create_remember_token
+    self.remember_token = User.hash(User.new_remember_token)
+  end
 end
